ecc27275fcb369e7ff991a4494824f1d404c457a
Gleidsong <gleidson.guilhem@gmail.com>
2018-03-15 20:53:03 -0300

    First Commit - Fibo

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,11 +2,13 @@
 public class Fibonacci{
 	
 	static long fibo(int x) {
+	static long fibo(int n) {
 		
 		int f = 0;
 		int ant = 0;
 		
 		for(int i = 1; i <= x; i++) {
+		for(int i = 1; i <= n; i++) {
 			if(i == 1) {
 				f = 1;
 				ant = 0;
@@ -15,7 +17,11 @@ public class Fibonacci{
 				ant = f-ant;
 			}
 			
+				ant = f - ant;
+			}			
 		}
 		return f;
+		return f;		
 	}
+	
 }
